a88fb459726bf8bf2551aca7e348ede28ad6342a
pyltsin <pyltsin-m@yandex.ru>
2017-07-29 07:45:13 +0300

    5 add few test

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -10,11 +10,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.DbPopulator;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.testng.Assert.*;
 import static ru.javawebinar.topjava.MealTestData.MATCHER;
 import static ru.javawebinar.topjava.MealTestData.MEAL_USER;
+import static ru.javawebinar.topjava.MealTestData.MEAL_USER2;
+import static ru.javawebinar.topjava.UserTestData.ADMIN;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration({"classpath:spring/spring-app.xml",
@@ -42,18 +52,34 @@ public class MealServiceTest {
         MATCHER.assertEquals(MEAL_USER, meal);
     }
 
+    @Test(expected = NotFoundException.class)
+    public void testGetNotFound() throws Exception {
+        service.get(1, 1);
+    }
+
+
     @Test
     public void testDelete() throws Exception {
-        
+        service.delete(MealTestData.MEAL_ID_START, UserTestData.USER_ID);
+        MealTestData.MATCHER.assertCollectionEquals(Collections.singletonList(MEAL_USER2), service.getAll(UserTestData.USER_ID));
     }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        service.delete(1,1);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        Collection<Meal> all = service.getAll(USER_ID);
+        MealTestData.MATCHER.assertCollectionEquals(Arrays.asList(MEAL_USER2, MEAL_USER), all);
+    }
+
 
     @Test
     public void testGetBetweenDateTimes() throws Exception {
     }
 
-    @Test
-    public void testGetAll() throws Exception {
-    }
 
     @Test
     public void testUpdate() throws Exception {
