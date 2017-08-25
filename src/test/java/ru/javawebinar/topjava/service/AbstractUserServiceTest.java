1f55ec78b9f9968e49ebb7e1d776422479f73c38
Mike <pma88@list.ru>
2017-08-25 14:36:39 +0300

    6 12 tomcat pool jndi cargo

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -0,0 +1,97 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.JpaUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static ru.javawebinar.topjava.UserTestData.*;
+
+public abstract class AbstractUserServiceTest extends AbstractServiceTest {
+
+    @Autowired
+    protected UserService service;
+
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Before
+    public void setUp() throws Exception {
+        service.evictCache();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        User newUser = new User(null, "New", "new@gmail.com", "newPass", 1555, false, Collections.singleton(Role.ROLE_USER));
+        User created = service.create(newUser);
+        newUser.setId(created.getId());
+        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, newUser, USER), service.getAll());
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void testDuplicateMailCreate() throws Exception {
+        service.create(new User(null, "Duplicate", "user@yandex.ru", "newPass", Role.ROLE_USER));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        service.delete(USER_ID);
+        MATCHER.assertCollectionEquals(Collections.singletonList(ADMIN), service.getAll());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testNotFoundDelete() throws Exception {
+        service.delete(1);
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        User user = service.get(USER_ID);
+        MATCHER.assertEquals(USER, user);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetNotFound() throws Exception {
+        service.get(1);
+    }
+
+    @Test
+    public void testGetByEmail() throws Exception {
+        User user = service.getByEmail("user@yandex.ru");
+        MATCHER.assertEquals(USER, user);
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        Collection<User> all = service.getAll();
+        MATCHER.assertCollectionEquals(Arrays.asList(ADMIN, USER), all);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        User updated = new User(USER);
+        updated.setName("UpdatedName");
+        updated.setCaloriesPerDay(330);
+        service.update(updated);
+        MATCHER.assertEquals(updated, service.get(USER_ID));
+    }
+
+    @Test
+    public void testValidation() throws Exception {
+        validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, Collections.emptySet())), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, Collections.emptySet())), ConstraintViolationException.class);
+    }
+}
